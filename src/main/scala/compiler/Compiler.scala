0dd1e5f73fc6209e961e87036ad8297cb0139f0b
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-18 01:29:36 +0700

    Add test suite for program tokenization

--- src/main/scala/compiler/Compiler.scala
+++ src/main/scala/compiler/Compiler.scala
@@ -1,5 +1,10 @@
 package compiler
 
 object Compiler extends App {
-  
+  /** Tokenize the input program into valid BrainFuck's tokens.
+   *
+   *  @param program the string of the program to be tokenized
+   *  @return a sequence of tokens
+   */
+  def tokenize(program: String): Seq[Char] = Seq()
 }
