9066948be78431dbb018f8967d1a165353cf3994
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-17 23:06:53 +0700

    Add BrainFuck's required instructions and create basic Operation class

---
+++ src/main/scala/compiler/Instruction.scala
@@ -0,0 +1,12 @@
+package compiler
+
+object Instruction {
+  val INC_PTR         = ">"
+  val DEC_PTR         = "<"
+  val INC_VAL         = "+"
+  val DEC_VAL         = "-"
+  val PRINT           = "."
+  val READ            = ","
+  val OPEN_BRACKET    = "["
+  val CLOSED_BRACKET  = "]"
+}
---
+++ src/main/scala/compiler/Operation.scala
@@ -0,0 +1,3 @@
+package compiler
+
+class Operation(val instruction: String, val argument: Short) { }
