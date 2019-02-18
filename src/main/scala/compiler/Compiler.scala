27874e74a7cef2fc86332706bba7daf303616fa9
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-18 19:46:40 +0700

    Find matching closed bracket

--- src/main/scala/compiler/Compiler.scala
+++ src/main/scala/compiler/Compiler.scala
@@ -1,4 +1,6 @@
 package compiler
+
+import scala.annotation.tailrec
 /** Object for compiling BrainFuck's codes.
  *
  *  This object contains functions that deal with tokenizing and compiling BrainFuck's codes into machine-readable operations.
@@ -19,7 +21,37 @@ object Compiler extends App {
    *  @return the index of the matching closed bracket
    */
   @throws(classOf[InvalidSyntaxException])
-  def findMatchingClosedBracket(operations: Seq[Char], openIndex: Short): Short = -1
+  def findMatchingClosedBracket(operations: Seq[Char], openIndex: Int): Int = {
+    findMatchingClosedBracketAccumulator(operations, openIndex + 1)
+  }
+  /** Helper method for finding matching closed bracket.
+   *
+   *  @param operations the sequence of operations 
+   *  @param openIndex the index of the current open bracket
+   *  @param closedIndex the index of the possible closed bracket
+   *  @param depth the current depth of the loop
+   *  @return the index of the matching closed bracket
+   */
+  @tailrec
+  @throws(classOf[InvalidSyntaxException])
+  def findMatchingClosedBracketAccumulator(operations: Seq[Char], closedIndex: Int, depth: Int = 1): Int = {
+    depth match {
+      case 0 => closedIndex - 1
+      case _ => {
+        val Length = operations.length
+        closedIndex match {
+          case Length => throw new InvalidSyntaxException("Syntax Error: no matching closed bracket found!")
+          case _ => {
+            operations(closedIndex) match {
+              case Instruction.OPEN_BRACKET => findMatchingClosedBracketAccumulator(operations, closedIndex + 1, depth + 1)
+              case Instruction.CLOSED_BRACKET => findMatchingClosedBracketAccumulator(operations, closedIndex + 1, depth - 1)
+              case _ => findMatchingClosedBracketAccumulator(operations, closedIndex + 1, depth)
+            }
+          }
+        }
+      }
+    }
+  }
   /** Find an open bracket that matches with the current closed bracket.
    *
    *  @param operations the sequence of operations 
@@ -27,5 +59,5 @@ object Compiler extends App {
    *  @return the index of the matching open bracket
    */
   @throws(classOf[InvalidSyntaxException])
-  def findMatchingOpenBracket(operations: Seq[Char], closedIndex: Short): Short = -1
+  def findMatchingOpenBracket(operations: Seq[Char], closedIndex: Int): Int = -1
 }
--- src/main/scala/compiler/InvalidSyntaxException.scala
+++ src/main/scala/compiler/InvalidSyntaxException.scala
@@ -3,4 +3,4 @@ package compiler
  *
  *  This exception deals with errors in BrainFuck's syntax.
  */
-final class InvalidSyntaxException(val message: String, val cause: Throwable = None.orNull) extends Exception(message, cause)
+final class InvalidSyntaxException(val message: String) extends Exception(message)
