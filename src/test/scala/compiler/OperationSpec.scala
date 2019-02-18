3a658846722d4228ef1157cb4b906ce1f84d5fcc
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-18 19:06:29 +0700

    Add test suites for Operation class

--- src/test/scala/compiler/OperationSpec.scala
+++ src/test/scala/compiler/OperationSpec.scala
@@ -6,5 +6,29 @@ import org.scalatest.FunSuite
  *  Test suites for Operation class.
  */
 class OperationSpec extends FunSuite {
-
+  test("Reflexive Case:\nx.equals(x) should return 'true'") {
+    val x = new Operation(Instruction.INC_PTR, 1)
+    assert(x == x)
+  }
+  test("Symmetric Case:\nx.equals(y) should return 'true' if and only if y.equals(x) returns 'true'") {
+    val x = new Operation(Instruction.INC_PTR, 1)
+    val y = new Operation(Instruction.INC_PTR, 1)
+    assert(x == y && y == x)
+  }
+  test("Transitive Case:\nIf x.equals(y) returns 'true' and y.equals(z) returns 'true', then x.equals(z) should return 'true'") {
+    val x = new Operation(Instruction.INC_PTR, 1)
+    val y = new Operation(Instruction.INC_PTR, 1)
+    val z = new Operation(Instruction.INC_PTR, 1)
+    assert(x == y && y == z && x == z)
+  }
+  test("Inequality Case 1:\nDifferent operations should return 'false'") {
+    val x = new Operation(Instruction.INC_PTR, 1)
+    val y = new Operation(Instruction.DEC_PTR, 1)
+    assert(x != y)
+  }
+  test("Inequality Case 2:\nSame operations with different argument should return 'false'") {
+    val x = new Operation(Instruction.INC_PTR, 1)
+    val y = new Operation(Instruction.INC_PTR, 2)
+    assert(x != y)
+  }
 }
