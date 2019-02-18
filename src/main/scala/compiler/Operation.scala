948eb9363346c6bb7a7076d00af25742d3016cc8
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-18 18:54:13 +0700

    Setup Operation class and test suites

--- src/main/scala/compiler/Operation.scala
+++ src/main/scala/compiler/Operation.scala
@@ -1,6 +1,15 @@
 package compiler
 /** Operations for the machine to execute.
  *
- *  Each operation instance contains the BrainFuck's instruction and the corresponding argument.
+ *  Each Operation instance contains the BrainFuck's instruction and the corresponding argument.
  */
-class Operation(val instruction: String, val argument: Short) { }
+class Operation(val instruction: String, val argument: Short) {
+  /** Override 'hashCode' method. */
+  override def hashCode: Int = {
+    super.hashCode
+  }
+  /** Override 'equals' method. */
+  override def equals(other: Any): Boolean = {
+    super.equals(other)
+  }
+}
---
+++ src/test/scala/compiler/OperationSpec.scala
@@ -0,0 +1,10 @@
+package compiler
+
+import org.scalatest.FunSuite
+/** OperationSpec.
+ *
+ *  Test suites for Operation class.
+ */
+class OperationSpec extends FunSuite {
+
+}
