10f2c3e0f40b1e339acbfcd11a7437fc409421f5
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-19 21:01:56 +0700

    Fix bad testing conventions

--- src/test/scala/compiler/OperationSpec.scala
+++ src/test/scala/compiler/OperationSpec.scala
@@ -13,13 +13,16 @@ class OperationSpec extends FunSuite {
   test("Symmetric Case:\nx.equals(y) should return 'true' if and only if y.equals(x) returns 'true'") {
     val x = new Operation(Instruction.INC_PTR, 1)
     val y = new Operation(Instruction.INC_PTR, 1)
-    assert(x == y && y == x)
+    assert(x == y)
+    assert(y == x)
   }
   test("Transitive Case:\nIf x.equals(y) returns 'true' and y.equals(z) returns 'true', then x.equals(z) should return 'true'") {
     val x = new Operation(Instruction.INC_PTR, 1)
     val y = new Operation(Instruction.INC_PTR, 1)
     val z = new Operation(Instruction.INC_PTR, 1)
-    assert(x == y && y == z && x == z)
+    assert(x == y)
+    assert(y == z)
+    assert(x == z)
   }
   test("Inequality Case 1:\nDifferent operations should return 'false'") {
     val x = new Operation(Instruction.INC_PTR, 1)
