d0ff56a94eb918a8d7f22b16e74bedfc8676c3f2
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-19 22:37:08 +0700

    Add example BrainFuck program

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,19 @@
+/** Imports */
+import scala.io.Source
+import compiler.{Compiler, InvalidSyntaxException}
+import machine.Machine
+/** Run an example BrainFuck program => helloworld.bf */
 object Main extends App {
-  println("Hello, World!")
+  val fileName = "helloworld.bf"
+  val bufferedSource = Source.fromResource(fileName)
+  val program = bufferedSource.getLines.mkString
+  val machine = new Machine(System.in, System.out)
+  try {
+    val operations = Compiler.compile(program)
+    machine.execute(operations)
+  } catch {
+    case e: InvalidSyntaxException => println(e.getMessage)
+    case _: Throwable => println("Unknown Exception!")
+  }
+  bufferedSource.close
 }
