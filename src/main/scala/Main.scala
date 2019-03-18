f64b3df47f11010227cccd1cf136eefc9801042b
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-18 12:18:32 +0700

    Use Scala's Try/Success/Failure

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,20 @@
 /** Imports */
 import scala.io.Source
-import compiler.{Compiler, InvalidSyntaxException}
+import scala.util.{Failure, Success, Try}
+import compiler.Compiler
 import machine.Machine
 /** Run an example Brainfuck program => hello.bf */
 object Main extends App {
   val fileName = "hello.bf"
-  val bufferedSource = Source.fromResource(fileName)
-  val program = bufferedSource.getLines.mkString
-  val machine = new Machine(System.in, System.out)
-  try {
-    machine.execute(Compiler.compile(program))
-    machine.clear()
-  } catch {
-    case e: InvalidSyntaxException => println(e.getMessage)
-    case _: Throwable => println("Unknown Exception!")
-  } finally {
-    bufferedSource.close
+  Try(Source.fromResource(fileName)) match {
+    case Success(bufferedSource) =>
+      val program = bufferedSource.getLines.mkString
+      val machine = new Machine(System.in, System.out)
+      Try(machine.execute(Compiler.compile(program))) match {
+        case Success(_) => machine.clear()
+        case Failure(f) => println(f.getMessage)
+      }
+      bufferedSource.close()
+    case Failure(f) => println(f.getMessage)
   }
 }
